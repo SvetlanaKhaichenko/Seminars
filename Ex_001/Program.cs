368bb27f53cfdd718c85ae2729dadca35f60a446
Svetlana <svdorohova@gmail.com>
2022-08-15 17:30:39 +0300

    1 задача корень числа

---
+++ Ex_001/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 49, b = 4;
+int result = b * b;
+
+if (result == a)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является квадратом числа {b}");
+}
