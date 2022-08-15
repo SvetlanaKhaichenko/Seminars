7aa747470eb4b5354d042c9b183f72e36e911be5
«Svetlana <svdorohova@gmail.com>
2022-08-15 20:38:02 +0300

    Задача 2 по номеру недели

---
+++ Ex_002/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine ("Введите число от 1 до 7");
+int numberweek = Convert.ToInt32(Console.ReadLine());
+
+if (numberweek == 1)
+{
+  Console.WriteLine ("Monday");  
+}
+if (numberweek == 2)
+{
+  Console.WriteLine ("Tuesday");  
+}
+if (numberweek == 3)
+{
+  Console.WriteLine ("Wednesday");  
+}
+if (numberweek == 4)
+{
+  Console.WriteLine ("Thursday");  
+}
+if (numberweek == 5)
+{
+  Console.WriteLine ("Friday");  
+}
+if (numberweek == 6)
+{
+  Console.WriteLine ("Saturday");  
+}
+if (numberweek == 7)
+{
+  Console.WriteLine ("Sunday");  
+}
