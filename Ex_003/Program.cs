f6fe4225d8857adc19c9ce2222f48adab1ead9f6
«Svetlana <svdorohova@gmail.com>
2022-08-15 21:16:30 +0300

    Задача с множествами

---
+++ Ex_003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number_0 = number * (-1);
+    Console.Write(number_0);
+    int count = number_0;
+
+while (count < number)
+{
+  count = count + 1;
+    Console.Write($" {count} ");
+}
