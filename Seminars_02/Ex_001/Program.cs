d4e56a41ae56bae4ccc32a6564ead6cea2d70d10
«Svetlana <svdorohova@gmail.com>
2022-08-16 21:28:49 +0300

    function

--- Seminars_02/Ex_001/Program.cs
+++ Seminars_02/Ex_001/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 2;
+int c1 = 8;
+int a2 = 512;
+int b2 = 12;
+int c2 = 38;
+int a3 = 2598;
+int b3 = 9;
+int c3 = 7;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max (
+ Max(a1, b1, c1),
+ Max(a2, b2, c2),
+ Max(a3, b3, c3));
+
+ 
+
+Console.WriteLine (max);
